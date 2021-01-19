64b5cf20f81a14d870070142249d645cf6fa9594
James Braddock Jr <jimbraddock07@yahoo.com>
2021-01-19 13:44:45 +0300

    development tools

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-
+/* Products Slider */
+$(".brand-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+}); 
+/* Products Slider Ends */
